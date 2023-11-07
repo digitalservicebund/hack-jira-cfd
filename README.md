a9cae2d60ead9c3832ccfe5d77ed38738810c803
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2023-11-07 12:08:50 +0100

    Do fetch with basic authentication

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # hack-jira-cfd
 Get more info about the flow of work in a Jira project.
 
-## How to Configure Authentication via Environment Variables
-* Export your email address as `ATLASSIAN_USER_EMAIL` or use an `.env` file to set it
-* Create an API token: [Atlassian docs](https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/)
-    *  Export as `ATLASSIAN_API_TOKEN` or use a `.env` file to set it
-## How to Run the App
+## Configure via Environment Variables
+We need a couple of environment variables set. You can use a `.env` file to do so.
+* `ATLASSIAN_USER_EMAIL` - The email of the account to authenticate with
+* `ATLASSIAN_API_TOKEN` - An API token of the account to authenticate with (here's how to create: [Atlassian docs](https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/))
+* `JIRA_API_BASE_URL` - The base URL of the Jira instance you want to talk to (e.g. `https://<myCompany>.atlassian.net/rest/api/2`)
+## Run the App
 * Fetch dependencies: `bun install`
 * Run once: `bun index.ts`
 * Run in watch mode: `bun --watch index.ts`
--- index.ts
+++ index.ts
@@ -1,1 +1,53 @@
-console.log("Hello via Bun!");
+console.log("Started");
+
+interface JiraData {
+    jiraApiBaseUrl: string,
+    jiraEmail: string,
+    jiraAuthToken: string,
+    jiraJqlQuery: string
+}
+
+const hardcodedJiraData: JiraData = {
+    jiraApiBaseUrl: process.env.JIRA_API_BASE_URL!,
+    jiraEmail: process.env.ATLASSIAN_USER_EMAIL!,
+    jiraAuthToken: process.env.ATLASSIAN_API_TOKEN!,
+    jiraJqlQuery: "project = NDISC AND status = Done AND created >= -30d order by created DESC"
+}
+
+// ensureDataIsComplete() // missing #thisIsAHack
+listHardcodedData(hardcodedJiraData)
+const body = await runJqlQueryAgainstJira(hardcodedJiraData)
+// check if we reached the limit of 50 results // missing #thisIsAHack
+
+console.log(body);
+
+
+
+// -----------
+function listHardcodedData(jiraData: JiraData): void {
+    console.log("jiraEmail:", jiraData.jiraEmail);
+    console.log("jiraAuthToken:", `...${jiraData.jiraAuthToken.slice(-5)}`);
+    console.log("jiraApiBaseUrl:", jiraData.jiraApiBaseUrl)
+    console.log("jiraJqlQuery:", `"${jiraData.jiraJqlQuery}"`);
+}
+
+async function runJqlQueryAgainstJira(jiraData: JiraData): Promise<object> {
+    const searchParams = new URLSearchParams({ "jql": jiraData.jiraJqlQuery })
+    const searchAPI = `${jiraData.jiraApiBaseUrl}/search`
+    const searchUrl = `${searchAPI}?${searchParams}`
+    console.log(searchUrl);
+
+    const base64Credentials = btoa(`${jiraData.jiraEmail}:${jiraData.jiraAuthToken}`)
+    const authHeaderValue = `Basic ${base64Credentials}`
+
+    const response = await fetch(searchUrl, {
+        method: "GET",
+        headers: { 
+            "Content-Type": "application/json", 
+            'Authorization': authHeaderValue
+        },
+    });
+
+    const body = await response.json();
+    return body
+}
