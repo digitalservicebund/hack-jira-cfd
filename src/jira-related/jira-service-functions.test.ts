4ab3ebb4e3df4c4ccc29799d47698c6b917dd03c
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2023-11-15 13:22:23 +0100

    Red test for content of createdDate and resolutionDate

--- src/jira-related/jira-service-functions.test.ts
+++ src/jira-related/jira-service-functions.test.ts
@@ -11,12 +11,14 @@ describe("mapJiraResponseToBusinessObjects()", () => {
         expect(result[0].key).toBeString();
     })
 
-    test("issues to have a 'createdDate' date property", () => {
+    test("issues to have a 'createdDate' date property with value '2023-11-02T10:02:34.255+0100'", () => {
         expect(result[0].createdDate).toBeDate();
+        expect(result[0].createdDate).toEqual(new Date("2023-11-02T10:02:34.255+0100"))
     })
 
-    test("issue to have a 'resolutionDate' date property", () => {
+    test("issue to have a 'resolutionDate' date property with value '2023-11-07T09:46:08.298+0100'", () => {
         expect(result[0].resolutionDate).toBeDate();
+        expect(result[0].resolutionDate).toEqual(new Date("2023-11-07T09:46:08.298+0100"))
     })
 })
 
