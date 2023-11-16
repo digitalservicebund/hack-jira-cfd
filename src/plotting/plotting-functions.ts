996a72fb4fea3f2c84c52210b5bd33f29d35a1fa
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2023-11-16 10:57:28 +0100

    Return single element of plotting data (empty, right now)

---
+++ src/plotting/plotting-functions.ts
@@ -0,0 +1,19 @@
+import { CycleTimeHistogramEntry } from "../core/core-functions";
+import { plot, Plot } from "nodeplotlib"
+
+export function plotCycleTimeHistogram(cycleTimeHistogramData: CycleTimeHistogramEntry[]): void {
+    const data: Plot[] = [
+        {
+            x: [1, 3, 4, 5],
+            y: [3, 12, 1, 4],
+            type: 'scatter',
+        },
+    ];
+
+    // plot(data);
+}
+
+export function createPlotDataFromCycleTimeHistogram(cycleTimeHistogram: CycleTimeHistogramEntry[]): Plot[] {
+    const result = [{}]
+    return result;
+}
