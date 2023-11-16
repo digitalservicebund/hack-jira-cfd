d76550b534c7c4fe042297425486cf43000a19c4
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2023-11-16 11:57:08 +0100

    Plot diagram

--- src/plotting/plotting-functions.ts
+++ src/plotting/plotting-functions.ts
@@ -1,16 +1,11 @@
+import { create } from "lodash";
 import { CycleTimeHistogramEntry } from "../core/core-functions";
 import { plot, Plot } from "nodeplotlib"
 
 export function plotCycleTimeHistogram(cycleTimeHistogramData: CycleTimeHistogramEntry[]): void {
-    const data: Plot[] = [
-        {
-            x: [1, 3, 4, 5],
-            y: [3, 12, 1, 4],
-            type: 'scatter',
-        },
-    ];
+    const data: Plot[] = createPlotDataFromCycleTimeHistogram(cycleTimeHistogramData)
 
-    // plot(data);
+    plot(data);
 }
 
 export function createPlotDataFromCycleTimeHistogram(cycleTimeHistogram: CycleTimeHistogramEntry[]): Plot[] {
