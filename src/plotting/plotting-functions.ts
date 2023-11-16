7cc46a7d95580f085ca9a44d42fc6ba4be40ea0c
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2023-11-16 12:06:16 +0100

    Return single Plot object (not array)

--- src/plotting/plotting-functions.ts
+++ src/plotting/plotting-functions.ts
@@ -3,19 +3,21 @@ import { CycleTimeHistogramEntry } from "../core/core-functions";
 import { plot, Plot, Layout } from "nodeplotlib"
 
 export function plotCycleTimeHistogram(cycleTimeHistogramData: CycleTimeHistogramEntry[]): void {
-    const data: Plot[] = createPlotDataFromCycleTimeHistogram(cycleTimeHistogramData)
+    const data: Plot[] = [
+        createPlotDataFromCycleTimeHistogram(cycleTimeHistogramData)
+    ]
 
     plot(data);
 }
 
-export function createPlotDataFromCycleTimeHistogram(cycleTimeHistogram: CycleTimeHistogramEntry[]): Plot[] {
+export function createPlotDataFromCycleTimeHistogram(cycleTimeHistogram: CycleTimeHistogramEntry[]): Plot {
     const xValues = cycleTimeHistogram.map(entry => entry.numberOfDays)
     const yValues = cycleTimeHistogram.map(entry => entry.issueCount)
-    const result: Plot[] = [{
+    const result: Plot = {
         x: xValues,
         y: yValues,
         type: "bar"
-    }]
+    }
 
     return result;
 }
