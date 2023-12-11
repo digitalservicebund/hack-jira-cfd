bfb3022d82630fc27e14e796391d75ba4b31d719
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2023-12-11 15:31:44 +0100

    Allow any for now

--- src/jira-related/jira-service-functions.ts
+++ src/jira-related/jira-service-functions.ts
@@ -8,7 +8,9 @@ export function createAuthorizationHeaderValue(jiraAuthEmail: string, jiraAuthTo
     return authHeaderValue;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function mapJiraResponseToBusinessObjects(jiraResponse: any): Issue[] {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const issues: Issue[] = jiraResponse.issues.map((i: any) => {
         return {
             key: i.key,
@@ -22,12 +24,15 @@ export function mapJiraResponseToBusinessObjects(jiraResponse: any): Issue[] {
 
 // https://digitalservicebund.atlassian.net/rest/api/2/issue/ndisc-40/changelog
 export function getDateForStartingInProgressOfIssue(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     issueChangelog: any,
     todoStateString: string,
     inProgressStateString: string): Date | undefined {
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const values: any[] = issueChangelog.values
     // can we assume just one? #thisIsAHack
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const valueWithItemsFromToDoToInProgress = values.find(value => _.some(value.items, (item: any) => itemIsTransitionToInProgress(item, todoStateString, inProgressStateString)))
 
     if (!valueWithItemsFromToDoToInProgress)
@@ -36,14 +41,17 @@ export function getDateForStartingInProgressOfIssue(
     return new Date(valueWithItemsFromToDoToInProgress.created);
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function itemComesFromState(item: any, stateString: string): boolean {
     return item.fromString === stateString
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function itemGoesToState(item: any, stateString: string): boolean {
     return item.toString === stateString
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function itemIsTransitionToInProgress(item: any, todoStateString: string, inProgressStateString: string): boolean {
     return itemComesFromState(item, todoStateString) && itemGoesToState(item, inProgressStateString)
 }
@@ -53,12 +61,17 @@ export interface StateWithDate {
     stateReachedDate: Date
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getAllStateChangesWithDates(issueChangelog: any): StateWithDate[] {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const valuesWithStateChanges = issueChangelog.values.filter((v: any) => _.some(v.items, (item: any) => item.fieldId === "status"))
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const stateChanges: StateWithDate[] = _.flattenDeep(valuesWithStateChanges.map((v: any) => {
         const items = v.items
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const itemWithStateInfo = items.filter((i: any) => i.fieldId === "status")
 
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         return itemWithStateInfo.map((i: any) => {
             return <StateWithDate>{
                 stateName: i.toString,
@@ -70,6 +83,7 @@ export function getAllStateChangesWithDates(issueChangelog: any): StateWithDate[
     return stateChanges
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getAllStatesWithDates(issue: Issue, issueChangelog: any): StateWithDate[] {
     const initialState: StateWithDate = {
         stateName: "Created",
