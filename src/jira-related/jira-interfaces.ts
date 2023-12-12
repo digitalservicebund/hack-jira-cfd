c5a1edd0d77c2b8f66d727cd267868d7e204b417
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2023-12-12 08:20:49 +0100

    Add interface for changelog value

--- src/jira-related/jira-interfaces.ts
+++ src/jira-related/jira-interfaces.ts
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export interface JiraChangelog {
-    values: {
-        created: string,
-        items: {
-            fromString: string | null,
-            toString: string,
-            [key: string]: any;
-        }[],
+    values: JiraChangelogValue[],
+    [key: string]: any;
+}
+
+export interface JiraChangelogValue {
+    created: string,
+    items: {
+        fromString: string | null,
+        toString: string,
         [key: string]: any;
     }[],
     [key: string]: any;
